7c58efd3f5f4441f2977dd6adfd9e91bcfb290e7
A <498338021@qq.com>
2018-04-08 15:53:11 +0800

    To configure gradle

--- app/src/main/java/com/zyn/jenkinstest2/MainActivity.java
+++ app/src/main/java/com/zyn/jenkinstest2/MainActivity.java
@@ -2,12 +2,17 @@ package com.zyn.jenkinstest2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_content = findViewById(R.id.tv_content);
+        tv_content.setText(BuildConfig.BASE_URL);
     }
 }
